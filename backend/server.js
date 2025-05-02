cfeca21200768e5f96102b9b4d56a077a4adc6fa
unknown <lawrencecaj@gmail.com>
2025-05-02 19:34:52 -0400

    base code

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on http://localhost:5000");
+});
+
+// Zr24fo9dCHQQvCei
