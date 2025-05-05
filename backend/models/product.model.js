1c43a4e784d0c6c13d5e4934cfc483e0ea156284
Lawrence <lawrencecaj@gmail.com>
2025-05-05 19:26:24 -0400

    controllers and routes

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,15 +1,25 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
-  {
-    name: { string, required: true },
-    price: { Number, required: true },
-    Image: { string, required: true },
-  },
-  { timestamps: true } //createdAt, updatedAt
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true, // createdAt, updatedAt
+	}
 );
 
 const Product = mongoose.model("Product", productSchema);
-// will pluralize the model name to create the collection name
-// e.g. Product -> products
+
 export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // Middleware to parse JSON data in req.body
+const __dirname = path.resolve();
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-  if (product.name || product.price || product.Image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please fill all the fields" });
-  }
+app.use("/api/products", productRoutes);
 
-  const newproduct = new Product(product);
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
-  try {
-    await newproduct.save();
-    res.status(201).json({ success: true, data: newproduct });
-  } catch (error) {
-    console.error("Error in saving product:", error.message);
-    res.status(500).json({ success: false, message: "Server error" });
-  }
-});
-
-app.listen(5000, () => {
-  connectDB();
-  console.log("Server is running on http://localhost:5000");
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
