79052cba5a0fba6268eec12e3b21faa0015d8923
Lawrence <lawrencecaj@gmail.com>
2025-05-06 14:17:30 -0400

    create page crazy page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Box } from '@chakra-ui/react'
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+	return (
+		<Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
 			<Navbar />
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/create' element={<CreatePage />} />
 			</Routes>
-		</Box> 
-  )
+		</Box>
+	);
 }
 
-export default App
+export default App;
