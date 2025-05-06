deaaed554c85b3d2eb3baffda696511849ed0e4f
Lawrence <lawrencecaj@gmail.com>
2025-05-06 11:30:34 -0400

    next day

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Box } from '@chakra-ui/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/create' element={<CreatePage />} />
+			</Routes>
+		</Box> 
   )
 }
 
